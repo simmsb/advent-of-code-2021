40f128c135098c1723a47b7186d2761b6a6016b8
Ben Simms <ben@bensimms.moe>
2021-11-30 18:34:37 +0000

    Get input stuff setup

--- src/day1/main.ml
+++ src/day1/main.ml
@@ -1,1 +1,12 @@
-let () = Printf.printf "day1! %s\n" (Sys.getcwd())
+open Core
+
+let day_name = "day1"
+
+let day_input =
+  let path =
+    Fpath.(
+      v (Sys.getcwd ()) / "aoc_inputs" / day_name |> set_ext "txt" |> to_string)
+  in
+  In_channel.read_lines path |> List.map ~f:String.strip
+
+let () = Printf.printf "day1! %s\n" String.(concat ~sep:"\n" day_input)
