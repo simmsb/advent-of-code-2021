df0b1599e33a693155fcf35830b4486b3cf510a7
simmsb <ben@bensimms.moe>
2021-12-25 17:16:29 +0000

    Deploying to gh-pages from  @ 6ba2a2a9a2ccf6f0ae0328eb1b1add00a655590a 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (742d223) (2021/12/07 19:38)',
+    VERSION: 'master (6ba2a2a) (2021/12/25 17:16)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
